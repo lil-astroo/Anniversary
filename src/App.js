4ca64e4b57c8641e34a934e3bdb2a2f8438d18e1
mani rahmati <astrroeffect@gmail.com>
2025-12-30 23:11:19 +0330

    last day

--- src/App.js
+++ src/App.js
@@ -53,8 +53,8 @@ export default function App() {
                 type: "image",
                 options: {
                     image: [
-                        { src: '%PUBLIC_URL%/img/heart.png'},
-                        { src: '%PUBLIC_URL%/img/fireheart.png'},
+                        { src: '/img/heart.png'},
+                        { src: '/img/fireheart.png'},
                     ]
                 }
             },
